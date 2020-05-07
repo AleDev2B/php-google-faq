1a300723c0b39a6dd8371752f649a74ce8315e7a
Alessandro <alecritelli@gmail.com>
2020-05-07 16:46:46 +0200

    add css

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="it" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <title></title>
 
     <?php include_once "db.php";
@@ -10,15 +11,19 @@
   </head>
   <body>
 
+    <div class="container">
       <?php
 
       foreach ($googlefaq as $domanda) {
         $h1 = $domanda["domanda"];
         $p = $domanda["risposta"];
-        echo "<h1>$h1</h1>";
-        echo "<p> $p</p>";
+        echo
+        "<h1>$h1</h1>" .
+        "<p>$p</p>";
       }
        ?>
+    </div>
+
 
 
 
