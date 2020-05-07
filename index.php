ef894628bbce089b0f2ec8e6fd5aaba00b49f8fd
Alessandro <alecritelli@gmail.com>
2020-05-07 20:04:56 +0200

    added layout fix

--- index.php
+++ index.php
@@ -4,27 +4,43 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
     <title>Domande frequenti – Privacy e termini – Google</title>
-
     <?php include_once "db.php";
     ?>
 
   </head>
   <body>
     <header>
-      <div class="logo">
-        <a href="#">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt=""> </a>
-        <h2>Privacy e termini</h2>
+      <div class="header-container">
+        <div class="logo displayFlexClass">
+          <a href="#">
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt=""> </a>
+          <h2>Privacy e termini</h2>
+        </div>
+        <div class="prova displayFlexClass">
+          <ul>
+            <li>
+              <a href="#">Introduzione</a>
+            </li>
+            <li>
+              <a href="#">Norme sulla privacy</a>
+            </li>
+            <li>
+              <a href="#">Termini di servizio</a>
+            </li>
+            <li>
+              <a href="#">Tecnologie</a>
+            </li>
+            <li>
+              <a href="#">Domande frequenti</a>
+            </li>
+          </ul>
+          <ul>
+            <li><a href="#">Account Google</a></li>
+          </ul>
+        </div>
       </div>
-      <div class="logo">
-        <ul>
-          <li><a href="#">Introduzione</a></li>
-          <li><a href="#">Norme sulla privacy</a></li>
-          <li><a href="#">Termini di servizio</a></li>
-          <li><a href="#">Tecnologie</a></li>
-          <li><a href="#">Domande frequenti</a></li>
-        </ul>
-      </div>
+
+      <hr>
     </header>
 
     <div class="container">
